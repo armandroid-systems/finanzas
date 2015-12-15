66768fc7cf739fa3cce38301968c2a707e36c1c1
armando.dominguez <armando.dominguez@globant.com>
2015-12-15 10:12:11 -0600

    screens 2

---
+++ app/src/main/java/com/globant/finanazaspersonales/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.globant.finanazaspersonales.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.globant.finanazaspersonales.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
